dcbd05203dd9d05296e9a7d32cb641616430f675
Unknown <markusdittrich1@gmail.com>
2018-03-01 18:03:00 +0100

    added randomString Function

--- lib.php
+++ lib.php
@@ -311,5 +311,13 @@ function generateMovieRating($id, $rating){
   ", FILE_APPEND);
 }
 
+function generateRandomString($length = 10) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 ?>
-    
